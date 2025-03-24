5bc78ce337d9f4e85f54474b4c2316bdb3b408d2
Steven Chowdhury <steven.chowdhury@gmail.com>
2025-03-23 19:41:08 -0700

    Set app title, add placeholder data for list

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import { Text, View, FlatList, StyleSheet, Image } from "react-native";
 
 export default function Index() {
+  const styles = StyleSheet.create({
+    itemContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#eee',
+    },
+    image: {
+      width: 50,
+      height: 50,
+      marginRight: 10,
+    },
+    text: {
+      fontSize: 16,
+    },
+  });
+  
+  const data = [
+    { id: 1, title: 'Rick Sanchez', imageUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+    { id: 1, title: 'Rick Sanchez', imageUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+    { id: 1, title: 'Rick Sanchez', imageUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+    { id: 1, title: 'Rick Sanchez', imageUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+    { id: 1, title: 'Rick Sanchez', imageUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+  ]
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.itemContainer}>
+              <Image style={styles.image} source={{ uri: item.imageUrl}}/>
+              <Text>{item.title}</Text>
+            </View>
+            
+          )
+        }}
+      />
     </View>
   );
 }
